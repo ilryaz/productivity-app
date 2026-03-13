de15c0ae154bccba6415e85b8988cb159f958341
Ilya <ilryaz08@gmail.com>
2026-03-13 21:20:11 +0300

    Added automatic subject addition

--- plugins/school/page.py
+++ plugins/school/page.py
@@ -10,70 +10,32 @@ class SchoolPage(QWidget):
         super().__init__()
 
         self.notebook = Notebook("Exam preparation", 30)
-        
-        # Maths
-        self.maths_label = QLabel('Mathematics')
-
-        maths_lower_layout = QHBoxLayout()
-
-        self.maths_input = QLineEdit()
-        self.maths_input.setPlaceholderText('Write here')
-        self.maths_button = QPushButton('+')
-
-        maths_lower_layout.addWidget(self.maths_input)
-        maths_lower_layout.addWidget(self.maths_button)
-
-        # Informatics
-        self.informatics_label = QLabel('Informatics')
-
-        informatics_lower_layout = QHBoxLayout()
-
-        self.informatics_input = QLineEdit()
-        self.informatics_input.setPlaceholderText('Write here')
-        self.informatics_button = QPushButton('+')
-
-        informatics_lower_layout.addWidget(self.informatics_input)
-        informatics_lower_layout.addWidget(self.informatics_button)
-
-        # Russian language
-        self.russian_label = QLabel('Russian')
-
-        russian_lower_layout = QHBoxLayout()
-
-        self.russian_input = QLineEdit()
-        self.russian_input.setPlaceholderText('Write here')
-        self.russian_button = QPushButton('+')
-
-        russian_lower_layout.addWidget(self.russian_input)
-        russian_lower_layout.addWidget(self.russian_button)
-
-        # German language
-        self.german_label = QLabel('German')
-
-        german_lower_layout = QHBoxLayout()
-
-        self.german_input = QLineEdit()
-        self.german_input.setPlaceholderText('Write here')
-        self.german_button = QPushButton('+')
-
-        german_lower_layout.addWidget(self.german_input)
-        german_lower_layout.addWidget(self.german_button)
-
 
         layout = QVBoxLayout(self)
 
-        layout.addWidget(self.maths_label)
-        layout.addLayout(maths_lower_layout)
+        layout.addLayout(self.create_subject_block('Maths'))
 
-        layout.addWidget(self.informatics_label)
-        layout.addLayout(informatics_lower_layout)
+        layout.addLayout(self.create_subject_block('Informatics'))
 
-        layout.addWidget(self.russian_label)
-        layout.addLayout(russian_lower_layout)
+        layout.addLayout(self.create_subject_block('Russian'))
 
-        layout.addWidget(self.german_label)
-        layout.addLayout(german_lower_layout)
+        layout.addLayout(self.create_subject_block('German'))
 
-    # def add_hours(self, h):
-    #     self.notebook.add_hours(h)
-    #     self.update_label()
+    def create_subject_block(self, name):
+        label = QLabel(name)
+    
+        input_field = QLineEdit()
+        input_field.setPlaceholderText("Write here")
+
+        button = QPushButton("+")
+        button.setFixedWidth(40)
+
+        lower_layout = QHBoxLayout()
+        lower_layout.addWidget(input_field)
+        lower_layout.addWidget(button)
+
+        block_layout = QVBoxLayout()
+        block_layout.addWidget(label)
+        block_layout.addLayout(lower_layout)
+
+        return block_layout
