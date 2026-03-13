d3cce2b7c1228320bfd5b257bd56c6eec90820ec
Ilya <ilryaz08@gmail.com>
2026-03-13 21:12:36 +0300

    Added QLabel and QPushbutton and four subjects to SchoolPage

--- plugins/school/page.py
+++ plugins/school/page.py
@@ -1,4 +1,5 @@
-from PySide6.QtWidgets import QWidget, QLabel, QPushButton, QVBoxLayout, QLineEdit
+from PySide6.QtWidgets import (QWidget, QLabel, QPushButton,
+                               QVBoxLayout, QHBoxLayout, QLineEdit)
 from PySide6.QtCore import Qt
 
 from .model import Notebook
@@ -9,35 +10,70 @@ class SchoolPage(QWidget):
         super().__init__()
 
         self.notebook = Notebook("Exam preparation", 30)
-
+        
+        # Maths
         self.maths_label = QLabel('Mathematics')
-        self.maths_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        maths_lower_layout = QHBoxLayout()
 
         self.maths_input = QLineEdit()
         self.maths_input.setPlaceholderText('Write here')
-        self.maths_input.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.maths_button = QPushButton('+')
 
-        # self.btn1 = QPushButton("+1 hour")
-        # self.btn2 = QPushButton("+2 hours")
+        maths_lower_layout.addWidget(self.maths_input)
+        maths_lower_layout.addWidget(self.maths_button)
 
-        # self.btn1.clicked.connect(lambda: self.add_hours(1))
-        # self.btn2.clicked.connect(lambda: self.add_hours(2))
+        # Informatics
+        self.informatics_label = QLabel('Informatics')
+
+        informatics_lower_layout = QHBoxLayout()
+
+        self.informatics_input = QLineEdit()
+        self.informatics_input.setPlaceholderText('Write here')
+        self.informatics_button = QPushButton('+')
+
+        informatics_lower_layout.addWidget(self.informatics_input)
+        informatics_lower_layout.addWidget(self.informatics_button)
+
+        # Russian language
+        self.russian_label = QLabel('Russian')
+
+        russian_lower_layout = QHBoxLayout()
+
+        self.russian_input = QLineEdit()
+        self.russian_input.setPlaceholderText('Write here')
+        self.russian_button = QPushButton('+')
+
+        russian_lower_layout.addWidget(self.russian_input)
+        russian_lower_layout.addWidget(self.russian_button)
+
+        # German language
+        self.german_label = QLabel('German')
+
+        german_lower_layout = QHBoxLayout()
+
+        self.german_input = QLineEdit()
+        self.german_input.setPlaceholderText('Write here')
+        self.german_button = QPushButton('+')
+
+        german_lower_layout.addWidget(self.german_input)
+        german_lower_layout.addWidget(self.german_button)
+
 
         layout = QVBoxLayout(self)
-        layout.addWidget(self.maths_label)
-        layout.addWidget(self.maths_input)
-        # layout.addWidget(self.btn1)
-        # layout.addWidget(self.btn2)
 
-        # self.update_label()
+        layout.addWidget(self.maths_label)
+        layout.addLayout(maths_lower_layout)
+
+        layout.addWidget(self.informatics_label)
+        layout.addLayout(informatics_lower_layout)
+
+        layout.addWidget(self.russian_label)
+        layout.addLayout(russian_lower_layout)
+
+        layout.addWidget(self.german_label)
+        layout.addLayout(german_lower_layout)
 
     # def add_hours(self, h):
     #     self.notebook.add_hours(h)
     #     self.update_label()
-
-    # def update_label(self):
-    #     percent = self.notebook.progress()
-    #     self.label.setText(
-    #         f"{self.notebook.current_hours}/{self.notebook.target_hours}\n"
-    #         f"{percent:.1f}%"
-    #     )
